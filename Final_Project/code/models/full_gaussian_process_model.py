8620921e7188782460c930f8fddcf406c1e0f489
Fabian-Leal <fabianlealv@gmail.com>
2022-06-20 10:49:54 +1000

    'map_cropping'

--- Final_Project/code/models/full_gaussian_process_model.py
+++ Final_Project/code/models/full_gaussian_process_model.py
@@ -22,21 +22,25 @@ class FullGaussianProcessModel(EcoacousticModel):
         self.gp = GaussianProcessRegressor(self.kernel, n_restarts_optimizer=15)
         
     def train(self, X, y):
-        self.filename = self.path + 'full_gp_'+str(np.unique(X[:,0]).size)+'_stationsnewkernel.gif'
-        self.filename_MSE = self.path + 'full_gp_'+str(np.unique(X[:,0]).size)+'_stationsnewkernel2_MSE.gif'
+        self.filename = self.path + 'full_gp_'+str(np.unique(X[:,0]).size)+'_stationsnewkernelhalfcrop2.gif'
+        self.filename_MSE = self.path + 'full_gp_'+str(np.unique(X[:,0]).size)+'_stationsnewkernel2_MSEhalfcrop.gif'
         self.X = X
         self.y = y
         self.gp.fit(X, y)
         self.trained = True
 
     def predict(self, x1x2x3, plot = False):
+        self.x1x2x3 = x1x2x3
         if self.trained:
             #y_pred, MSE = self.gp.predict(x1x2x3, return_cov=True)
             y_pred, MSE = self.gp.predict(x1x2x3, return_std=True)
-
-            y_lower = y_pred - 2*MSE
-            y_higher = y_pred + 2*MSE
-
+            
+            y_lower1 = y_pred - 2*MSE
+            y_higher1 = y_pred + 2*MSE
+            
+            y_lower = [item if item >= 0 else 0 for item in y_lower1]
+            y_higher = [item if item >= 0 else 0 for item in y_higher1]
+            
             self.vmin = np.min(y_lower)
             self.vmax = np.max(y_higher)
 
@@ -49,6 +53,7 @@ class FullGaussianProcessModel(EcoacousticModel):
             wide_raw_data.columns.name = None
                         
             pred_df = pd.DataFrame({'y_pred':y_pred,'y_lower':y_lower,'y_higher':y_higher,'MSE':MSE})
+            pred_df[pred_df < 0] = 0
             cov_df = pd.DataFrame({'long':x1x2x3[:,0],'lat':x1x2x3[:,1],'time':x1x2x3[:,2]})
 
             df = pd.concat([pred_df, cov_df], axis=1)
@@ -88,6 +93,9 @@ class FullGaussianProcessModel(EcoacousticModel):
         
         fig1, ax1 = plt.subplots(1,4,figsize=(20,4),gridspec_kw={"width_ratios":[1,1,1,0.05]})
         fig1.subplots_adjust(wspace=0.3)
+        
+        #for row in ax1:
+        #    row.plot(self.x1x2x3[:,0],self.x1x2x3[:,1])
 
         ax1[0].title.set_text('-2σ')
         ax1[0].set_xlabel('Longitude')
@@ -97,19 +105,18 @@ class FullGaussianProcessModel(EcoacousticModel):
         ax1[1].set_xlabel('Longitude')
         ax1[1].set_ylabel('Latitude')        
 
-        ax1[2].title.set_text('-2σ')
+        ax1[2].title.set_text('+2σ')
         ax1[2].set_xlabel('Longitude')
         ax1[2].set_ylabel('Latitude') 
         
-        lower_plot = ax1[0].pcolormesh(X0, X1, np.reshape(lower[lower.columns[0]].values,(n_points,n_points)),vmin=self.vmin,vmax=self.vmax,cmap='RdBu_r',shading='auto')
-        lower_plot_scatter = ax1[0].scatter(raw_long_lats[:,0], raw_long_lats[:,1], s = raw_data[raw_data.columns[0]].values*100,c='g')
+        lower_plot = ax1[0].pcolormesh(X0, X1, np.reshape(lower[lower.columns[0]].values,(n_points,n_points)),vmin=self.vmin,vmax=self.vmax,cmap='Greens',shading='auto')
+        lower_plot_scatter = ax1[0].scatter(raw_long_lats[:,0], raw_long_lats[:,1], s = raw_data[raw_data.columns[0]].values*100,c='b')
 
-        pred_plot = ax1[1].pcolormesh(X0, X1, np.reshape(pred[pred.columns[0]].values,(n_points,n_points)),vmin=self.vmin,vmax=self.vmax,cmap='RdBu_r',shading='auto')
-        pred_plot_scatter = ax1[1].scatter(raw_long_lats[:,0], raw_long_lats[:,1], s = raw_data[raw_data.columns[0]].values*100,c='g')
+        pred_plot = ax1[1].pcolormesh(X0, X1, np.reshape(pred[pred.columns[0]].values,(n_points,n_points)),vmin=self.vmin,vmax=self.vmax,cmap='Greens',shading='auto')
+        pred_plot_scatter = ax1[1].scatter(raw_long_lats[:,0], raw_long_lats[:,1], s = raw_data[raw_data.columns[0]].values*100,c='b')
         
-        higher_plot = ax1[2].pcolormesh(X0, X1, np.reshape(higher[higher.columns[0]].values,(n_points,n_points)),vmin=self.vmin,vmax=self.vmax,cmap='RdBu_r',shading='auto')
-        higher_plot_scatter = ax1[2].scatter(raw_long_lats[:,0], raw_long_lats[:,1], s = raw_data[raw_data.columns[0]].values*100,c='g')
-        
+        higher_plot = ax1[2].pcolormesh(X0, X1, np.reshape(higher[higher.columns[0]].values,(n_points,n_points)),vmin=self.vmin,vmax=self.vmax,cmap='Greens',shading='auto')
+        higher_plot_scatter = ax1[2].scatter(raw_long_lats[:,0], raw_long_lats[:,1], s = raw_data[raw_data.columns[0]].values*100,c='b')
         fig1.colorbar(pred_plot, cax=ax1[3])
         
         title_1 = fig1.suptitle(format_title(pred.columns[0]))
@@ -125,6 +132,7 @@ class FullGaussianProcessModel(EcoacousticModel):
             pred_plot_scatter.set_sizes(raw_data[raw_data.columns[i+1]].values*100)
             
             title_1.set_text(format_title(pred.columns[i+1]))
+             
             return lower_plot,pred_plot,higher_plot
         
         anim = FuncAnimation(fig1, animate, interval=500, frames=pred.shape[1] - 1,blit=False)
@@ -135,6 +143,7 @@ class FullGaussianProcessModel(EcoacousticModel):
         ax2.title.set_text('MSE')
         ax2.set_xlabel('Longitude')
         ax2.set_ylabel('Latitude')
+
         
         MSE_plot = ax2.pcolormesh(X0, X1, np.reshape(MSE[MSE.columns[0]].values,(n_points,n_points)),vmin=self.vmin_MSE,vmax=self.vmax_MSE,cmap='RdBu_r',shading='auto')        
         title_2 = fig2.suptitle(format_title(MSE.columns[0]))
